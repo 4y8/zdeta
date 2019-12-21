1d562ee86f2278c48093c834149321fa1d5b2a2f
TA4TA40S <aghilas.boussaa@laposte.net>
2019-12-21 10:07:57 +0100

    Continued the lexer.

--- zdc.c
+++ zdc.c
@@ -3,21 +3,21 @@
 #include <ctype.h>
 #include <string.h>
 
-enum type{operator, terminator, identifier, number};
+enum type{operator, separator, identifier, number};
 struct token {
     enum type type;
     char value[50];
 };
 
-char symbols[9] = {'(',')','{','}',';','"','[',']',','};
+char symbols[10]= {'(',')','{','}',';','"','[',']',',','#'};
 char operators[8] = {'>','<', '=', '!', '+', '/', '-', '%'};
-char *funs[13] = {"let", "fun", "print", "while", "if", "else",
+char *keywords[13] = {"let", "fun", "print", "while", "if", "else",
                   "elif", "var", "swap", "case", "switch", "iter"}; // The list of all functions
 struct token *tokens;
 
-int isfun(char in[]){
+int iskeyword(char in[]){
     for(int i = 0; i < 12; i++){
-        if (strcmp(funs[i], in) == 0){
+        if (strcmp(keywords[i], in) == 0){
             return 1;
         }
     }
@@ -25,7 +25,7 @@ int isfun(char in[]){
 }
 
 int isinchars(char in[], char check){
-    for(int i = 0; i < 13; i++){
+    for(int i = 0; i < 10; i++){
         if (in[i] == check){
             return 1;
         }
@@ -34,7 +34,7 @@ int isinchars(char in[], char check){
 }
 
 void lexer(FILE *fp1){
-    tokens = (struct token*) malloc(10 * sizeof(struct token));
+    tokens = (struct token*) malloc(100 * sizeof(struct token));
     char c = 'a';
     int i = 0;
     int j = 0;
@@ -57,7 +57,8 @@ void lexer(FILE *fp1){
             fseek(fp1, l - 1, SEEK_SET);
             (tokens+k)->type = 2;
             strcpy((tokens+k)->value, buffer);
-            k ++;
+            puts((tokens+k)->value);
+            k++;
         }
         else if (isdigit(c)){
             j = ftell(fp1);
@@ -72,109 +73,39 @@ void lexer(FILE *fp1){
             l = ftell(fp1);
             fseek(fp1, j - 1, SEEK_SET);
             fgets(buffer, i + 1, fp1);
-            fseek(fp1, l - 1, SEEK_SET);
+            fseek(fp1, l - 2, SEEK_SET);
             if ((tokens+k)->type != 2){
                 (tokens+k)->type = 3;
             }
             strcpy((tokens+k)->value, buffer);
-            k ++;
+            puts((tokens+k)->value);
+            k++;
         }
-        else {
-            if (isinchars(operators, c)){
-                strcpy(pars[k].type, "Opp");
-                if(c == '/'){
-                    c = fgetc(fp1);
-                    if(c == '/'){
-                        while (c != '\n'){
-                            c = fgetc(fp1);
-                        }
-                        fseek(fp1, -1, SEEK_CUR);
-                    }
-                    else{
-                        conv[0] = '/';
-                        strcpy(pars[k].name, conv);
-                    }
-                }
-                else{
-                    conv[0] = c;
-                    strcpy(pars[k].name, conv);
-                }
-                k ++;
-            }
-            else if (isinchars(symbols, c)){
-                strcpy(pars[k].type, "Symb");
-                switch(c){
-                    case '"':
-                        strcpy(pars[k].name, "quote");
-                        break;
-                    case '[':
-                        strcpy(pars[k].name, "leftsquarebracket");
-                        break;
-                    case ']':
-                        strcpy(pars[k].name, "rightsquarebracket");
-                        break;
-                    case ';':
-                        strcpy(pars[k].name, "semicolon");
-                        break;
-                    case '(':
-                        strcpy(pars[k].name, "leftparenthesis");
-                        break;
-                    case ')':
-                        strcpy(pars[k].name, "rightparenthesis");
-                        break;
-                    case ',':
-                        strcpy(pars[k].name, "coma");
-                        break;
-                    case '=':
-                        strcpy(pars[k].name, "equal");
-                        break;
-                    case '<':
-                        strcpy(pars[k].name, "lowerthan");
-                        break;
-                    case '>':
-                        strcpy(pars[k].name, "greaterthan");
-                        break;
-                    case '!':
-                        strcpy(pars[k].name, "exclam");
-                        break;
-                    case '%':
-                        strcpy(pars[k].name, "mod");
-                        break;
-                }
-                k ++;
-            }
+        else if (isinchars(operators, c)){
+            (tokens+k)->type = 0;
+            conv[0] = c;
+            strcpy((tokens+k)->value, conv);
+            puts((tokens+k)->value);
+            k++;
         }
-        if(c == '\n'){
-            if(strcmp(pars[k - 1].name, "Ter") != 0){
-                strcpy(pars[k].type, "Ter");
-                strcpy(pars[k].name, "Ter");
-                k ++;
-                i = 0;
-                strcpy(pars[k].type, "Spc");
-                if (isspace(c)){
-                    while(isspace(c)){
-                        c = fgetc(fp1);
-                        i ++;
-                    }
-                    fseek(fp1, -1, SEEK_CUR);
-                    sprintf(pars[k].name, "%d", i);
-                }
-                k ++;
-            }
+        else if (isinchars(symbols, c)){
+            (tokens+k)->type = 1;
+            conv[0] = c;
+            strcpy((tokens+k)->value, conv);
+            puts((tokens+k)->value);
+            k++;
+        }
+        else if (c == '\n'){
+            (tokens+k)->type = 1;
+            strcpy((tokens+k)->value, "terminator");
+            puts((tokens+k)->value);
+            k++;
         }
     }
 }
 
-
 int main( int argc, char *argv[] ){
     FILE *fp1 = fopen (argv[1], "r");
-    FILE *fp2 = fopen("testhello.c", "w+");
-    fputs ("#include <stdio.h>\n", fp2);
-    fputs ("int main(){\n", fp2);
-    char c = 'a';
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    int l = 0;
     lexer(fp1);
+    return 0;
 }
